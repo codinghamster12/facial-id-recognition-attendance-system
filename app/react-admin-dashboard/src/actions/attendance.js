e230610e4cfe0736a770bdb9ab109c83e7343d63
Bushra Akram <bushra.akram999@hotmail.com>
2021-06-24 23:16:18 +0500

    update-attendance

--- app/react-admin-dashboard/src/actions/attendance.js
+++ app/react-admin-dashboard/src/actions/attendance.js
@@ -1,153 +1,131 @@
-import { attendanceConstants } from './constants'
-import axios from '../helpers/axios';
+import { attendanceConstants } from "./constants";
+import axios from "../helpers/axios";
 
+export const getAttendance = (id, class_id) => {
+  return async (dispatch) => {
+    dispatch({
+      type: attendanceConstants.STUDENT_ATTENDANCE_REQUEST,
+    });
+    const token = localStorage.getItem("token");
+    const headers = {
+      Authorization: `Token ${token}`,
+    };
+    // data= JSON.parse(data)
+    let config = {
+      headers: { Authorization: `Token ${token}` },
+      params: {
+        class_id: class_id,
+      },
+    };
+    // console.log(data)
+    try {
+      const res = await axios.get(`/classes/view-attendance/${id}/`, config);
 
-export const getAttendance = (id,class_id) => {
-
-    return async (dispatch) => {
+      if (res.status == 200) {
+        console.log(res.data);
         dispatch({
-            type: attendanceConstants.STUDENT_ATTENDANCE_REQUEST
-        })
-        const token= localStorage.getItem('token')
-        const headers= {
-            'Authorization': `Token ${token}`
-        }
-        // data= JSON.parse(data)
-        let config = {
-            headers:{'Authorization': `Token ${token}`},
-            params: {
-              class_id: class_id
-            },
-          }
-        // console.log(data)
-        try{
-
-            const res= await axios.get(`/classes/view-attendance/${id}/`,config)
-            
-            if(res.status == 200){
-                console.log(res.data)
-                dispatch({
-                    type: attendanceConstants.STUDENT_ATTENDANCE_SUCCESS,
-                    payload:{
-                        attendance: res.data
-                        
-                    }
-                })
-            }
-
-        }
-        catch(error){
-            // if(error.response.status == 400){
-            //     dispatch({
-            //         type: studentConstants.GET_STUDENTS_FAILURE,
-            //         payload:{
-            //             error: error.response.data
-            //         }
-            //     })
-            // }
-            console.log(error);
-        }
-
-       
-        
+          type: attendanceConstants.STUDENT_ATTENDANCE_SUCCESS,
+          payload: {
+            attendance: res.data,
+          },
+        });
+      }
+    } catch (error) {
+      // if(error.response.status == 400){
+      //     dispatch({
+      //         type: studentConstants.GET_STUDENTS_FAILURE,
+      //         payload:{
+      //             error: error.response.data
+      //         }
+      //     })
+      // }
+      console.log(error);
     }
-}
+  };
+};
 
 export const getClassAttendance = (class_id) => {
+  return async (dispatch) => {
+    dispatch({
+      type: attendanceConstants.CLASS_ATTENDANCE_REQUEST,
+    });
+    const token = localStorage.getItem("token");
+    const headers = {
+      Authorization: `Token ${token}`,
+    };
+    // data= JSON.parse(data)
+    let config = {
+      headers: { Authorization: `Token ${token}` },
+    };
+    // console.log(data)
+    try {
+      const res = await axios.get(
+        `/classes/view-classattendance/${class_id}/`,
+        config
+      );
 
-    return async (dispatch) => {
+      if (res.status == 200) {
+        console.log(res.data);
         dispatch({
-            type: attendanceConstants.CLASS_ATTENDANCE_REQUEST
-        })
-        const token= localStorage.getItem('token')
-        const headers= {
-            'Authorization': `Token ${token}`
-        }
-        // data= JSON.parse(data)
-        let config = {
-            headers:{'Authorization': `Token ${token}`},
-            
-          }
-        // console.log(data)
-        try{
-
-            const res= await axios.get(`/classes/view-classattendance/${class_id}/`,config)
-            
-            if(res.status == 200){
-                console.log(res.data)
-                dispatch({
-                    type: attendanceConstants.CLASS_ATTENDANCE_SUCCESS,
-                    payload:{
-                        attendance: res.data
-                        
-                    }
-                })
-            }
-
-        }
-        catch(error){
-            // if(error.response.status == 400){
-            //     dispatch({
-            //         type: studentConstants.GET_STUDENTS_FAILURE,
-            //         payload:{
-            //             error: error.response.data
-            //         }
-            //     })
-            // }
-            console.log(error);
-        }
-
-       
-        
+          type: attendanceConstants.CLASS_ATTENDANCE_SUCCESS,
+          payload: {
+            attendance: res.data,
+          },
+        });
+      }
+    } catch (error) {
+      // if(error.response.status == 400){
+      //     dispatch({
+      //         type: studentConstants.GET_STUDENTS_FAILURE,
+      //         payload:{
+      //             error: error.response.data
+      //         }
+      //     })
+      // }
+      console.log(error);
     }
-}
+  };
+};
 
 export const getTodayAttendance = (class_id) => {
+  return async (dispatch) => {
+    dispatch({
+      type: attendanceConstants.TODAY_ATTENDANCE_REQUEST,
+    });
+    const token = localStorage.getItem("token");
+    const headers = {
+      Authorization: `Token ${token}`,
+    };
+    // data= JSON.parse(data)
+    let config = {
+      headers: { Authorization: `Token ${token}` },
+    };
+    // console.log(data)
+    try {
+      const res = await axios.get(
+        `/classes/attendance/today/${class_id}/`,
+        config
+      );
 
-    return async (dispatch) => {
+      if (res.status == 200) {
+        console.log(res.data);
         dispatch({
-            type: attendanceConstants.TODAY_ATTENDANCE_REQUEST
-        })
-        const token= localStorage.getItem('token')
-        const headers= {
-            'Authorization': `Token ${token}`
-        }
-        // data= JSON.parse(data)
-        let config = {
-            headers:{'Authorization': `Token ${token}`},
-            
-          }
-        // console.log(data)
-        try{
-
-            const res= await axios.get(`/classes/attendance/today/${class_id}/`,config)
-            
-            if(res.status == 200){
-                console.log(res.data)
-                dispatch({
-                    type: attendanceConstants.TODAY_ATTENDANCE_SUCCESS,
-                    payload:{
-                        attendance: res.data
-                        
-                    }
-                })
-            }
-
-        }
-        catch(error){
-            // if(error.response.status == 400){
-            //     dispatch({
-            //         type: studentConstants.GET_STUDENTS_FAILURE,
-            //         payload:{
-            //             error: error.response.data
-            //         }
-            //     })
-            // }
-            console.log(error);
-        }
-
-       
-        
+          type: attendanceConstants.TODAY_ATTENDANCE_SUCCESS,
+          payload: {
+            attendance: res.data,
+          },
+        });
+      }
+    } catch (error) {
+      // if(error.response.status == 400){
+      //     dispatch({
+      //         type: studentConstants.GET_STUDENTS_FAILURE,
+      //         payload:{
+      //             error: error.response.data
+      //         }
+      //     })
+      // }
+      console.log(error);
     }
-}
-
+  };
+};
